4bca82f6bbfc5b48fede1e59e65558ac37dda504
Usama Ahmad <saamjzero@hotmail.com>
2024-12-31 22:16:35 +0500

    cognito auth - login only path

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,11 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyviteauthui297796dd297796dd": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
